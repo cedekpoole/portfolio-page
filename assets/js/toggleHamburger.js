4d9c226c86a28752347122f0e34e1a612f27a3cb
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-05 14:06:41 +0100

    fix hamburger menu

--- assets/js/toggleHamburger.js
+++ assets/js/toggleHamburger.js
@@ -4,15 +4,21 @@ const link = document.querySelectorAll(".nav-links li");
 const pageTitle = document.querySelector(".page-title");
 
 // if window is less than 992px, change page title
-if (window.innerWidth < 992) {
+if (window.innerWidth < 768) {
     pageTitle.innerText = "Cedekpoole";
 }
 
 // when window is resized, change page title
 window.addEventListener("resize", () => {
-  window.innerWidth > 992
+
+  window.innerWidth > 768
     ? (pageTitle.innerText = "Cameron Edek Poole")
     : (pageTitle.innerText = "Cedekpoole");
+
+    // if window is more than 992px, close hamburger menu
+  if (window.innerWidth >= 992) {
+    navLinks.classList.remove("show-nav-links");
+  }
 });
 
 // when hamburger menu is pressed, show navigation links
@@ -23,6 +29,8 @@ toggleButton.addEventListener("click", () => {
 // when each navigation link is pressed, close hamburger menu
 link.forEach((link) => {
   link.addEventListener("click", () => {
-    navLinks.classList.toggle("show-nav-links");
+    if (window.innerWidth < 992) {
+        navLinks.classList.toggle("show-nav-links");
+    }
   });
 });
